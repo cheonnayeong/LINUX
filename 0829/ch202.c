63e7e1b7be0240ad16a8403fa52d5a353109870d
천나영 <102145415+cheonnayeong@users.noreply.github.com>
2023-08-29 09:54:33 +0900

    디렉토리 삭제하기

---
+++ 0829/ch202.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() 
+{
+    if(rmdir("han") == -1)
+    {
+        perror("han");
+        exit(1);
+    }
+    printf("delete directory success!!\n");
+
+    return 0;
+}
